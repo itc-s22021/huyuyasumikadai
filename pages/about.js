f6f6a734c23e0da02a47aea8a77bed77b30a56bf
s22021 <s22021@std.it-college.ac.jp>
2023-01-11 09:49:34 +0000

    kannsei

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,45 @@
+import Container from '@/components/container'
 import Hero from '@/components/hero'
+import PostBody from '@/components/post-body'
+import Contact from '@/components/contact'
+import TwoColumn from '@/components/two-column'
 
 export default function About () {
   return (
-    <Hero
-      title='About'
-      subtitle='About development activities'
-    />
+    <Container>
+      <Hero title='About' subtitle='About development activities' />
+
+      <TwoColumn>
+        <TwoColumn.Main>
+          <PostBody>
+            <p>
+              Cube が得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、さ
+              まざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
+            </p>
+            <h2> モノづくりで目指していること </h2>
+            <p>
+              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入
+              れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提案が数多くありますが、
+              特にそこを磨く作業に力を入れています。
+            </p>
+            <p>
+              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしてい
+              ます。毎回課題解決テーマを持って「モノ」と向き合い制作をし、フィードバックしてもらうことで自分
+              の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
+            </p>
+            <h3> 新しいことへのチャレンジ </h3>
+            <p>
+              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから
+              取り入れて、良いものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづ
+              くりは、これからもつづけていきたいです。\
+            </p>
+          </PostBody>
+        </TwoColumn.Main>
+
+        <TwoColumn.Sidebar>
+          <Contact />
+        </TwoColumn.Sidebar>
+      </TwoColumn>
+    </Container>
   )
 }
